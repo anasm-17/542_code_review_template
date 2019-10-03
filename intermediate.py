f4d6789702ff9661f6b009518f9d457df5c599c2
anas017 <anas.m.017@gmail.com>
2019-10-03 10:49:28 -0700

    new change

--- intermediate.py
+++ intermediate.py
@@ -5,7 +5,7 @@
 def find_string_in_list(string_looking = "cherries", 
 						list_finding = ["apples", "oranges", "bananas", "grapes"] ):
     """ 
-    This function allegedly looks for string_looking in list_finding and returns the string_looking if the string is found in the link. But what does it do if it can't find the string? WHO KNOWS....
+    This function allegedly looks for string_looking in list_finding and returns the string_looking if the string is found in the link. But what does it do if it can't find the string? WHO KNOWS...
     """
 	found = False
 	size = len(arr)
